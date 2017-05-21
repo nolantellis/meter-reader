fea4ee393ca792a4323aeb5af672feeaf492da69
Nolan Tellis <nolan.tellis@intelematics.com>
2017-05-21 16:10:48 +1000

    minor refactroing changes

--- src/main/java/com/redenergy/entity/RecordType.java
+++ src/main/java/com/redenergy/entity/RecordType.java
@@ -6,10 +6,12 @@ public enum RecordType
 {
     START_RECORD(100), END_RECORD(900), START_METER_RECORD(200), METER_READ_RECORD(300);
     int num;
+
     private RecordType(int num)
     {
         this.num = num;
     }
+
     public static RecordType getRecordType(int num)
     {
         return Stream.of(RecordType.values()).filter(e -> e.num == num).findFirst().orElse(null);
